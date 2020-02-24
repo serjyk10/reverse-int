e6f7486a19eda80bac09008ac9542be512e63a97
Sergey Borisevich <borisevich.serjyk@gmail.com>
2020-02-24 16:53:59 +0300

    feat: task is solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    return +(n < 0 ? -n : n).toString().split('').reduceRight((finNum, cur) => finNum += cur, '');
 }
